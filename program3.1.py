e53ee92b376999b43bda3eed7553e8d305ee94c5
sharibatasnim101-dev <shariba.tasnim101@gmail.com>
2026-02-02 14:35:48 +0530

    Files deleted

--- program3.1.py
+++
@@ -1,7 +0,0 @@
-def calcSum(x,y):
-    s=x+y
-    return s
-num1=float(input("Enter 1st number: "))
-num2=float(input("Enter 2nd number: "))
-sum=calcSum(num1,num1)
-print("Sum of the two numbers is: ",sum)
--- squareofnum.py
+++
@@ -1,7 +0,0 @@
-#to find square of a number using functions
-def square(x):
-    s=x*x
-    return s
-num=int(input("Enter the number: "))
-sq=square(num)
-print("Square:",sq)
